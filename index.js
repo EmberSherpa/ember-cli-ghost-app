2b129236d8efb294d41e9c920ccb221c5b3a88db
Taras Mankovski <tarasm@gmail.com>
2015-01-31 16:47:56 -0500

    Initial commit of plugin code

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
+/* global require */
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-ghost-app'
+  name: 'ember-cli-ghost-app',
+  serverMiddleware: function(config) {
+    var ghost = require('ghost');
+    var path = require('path');
+
+    var app = config.app;
+    var options = config.options;
+    return ghost({
+      config: path.join(options.project.root, 'ghost.js')
+    })
+      .then(function (ghostServer) {
+        app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+        ghostServer.start(app);
+      });
+  }
 };
